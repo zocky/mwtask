967b1939b7c9ae547dde8fb86d0f75e630a2db88
zocky <zocky@nospam.please>
2017-08-12 01:16:58 +0200

    tests

--- index.js
+++ index.js
@@ -14,47 +14,63 @@ getopt = new Getopt([
 
 getopt.bindHelp();
 var opt = getopt.parseSystem(); 
-
-if (opt.options.toHtml) {
-
-  var text = fs.readFileSync(opt.options.toHtml,'utf8');
-  var parsed = require('./lib/parser-text').parse(text);
-
-  if (opt.options.parse) {
-    console.log(parsed);
-  } else {
-    var html = require('./lib/output-html').output(parsed);
-    console.log(html);
-  }
-  
-} else if (opt.options.fromHtml) {
-
-  var html = fs.readFileSync(opt.options.fromHtml,'utf8');
-  var parsed = require('./lib/parser-html').parse(html);
-
-  if (opt.options.parse) {
-    console.log(parsed);
-  } else {
-    var text = require('./lib/output-text').output(parsed);
-    console.log(text);
-  }
-  
-} else if (opt.options.oldHtml && opt.options.diff) {
-
-  var html = fs.readFileSync(opt.options.oldHtml,'utf8');
-  var parsed = require('./lib/parser-html').parse(html);
-  
-  var diff = fs.readFileSync(opt.options.diff,'utf8');
-  var changes = require('./lib/parser-diff').parse(diff);
-  var changed = require('./lib/patch').patch(parsed,changes);
-
-  if (opt.options.parse) {
-    console.log(changed);
-  } else {
-    var html = require('./lib/output-html').output(changed);
-    console.log(html);
+var output;
+if (opt.argv[0]) {
+  output = function(obj) {
+    fs.writeFileSync(
+      opt.argv[0],
+      typeof obj == 'string' ? obj : JSON.stringify(obj),
+      'utf8'
+    );
   }
 } else {
-  getopt.showHelp();
+  output = console.log.bind(console);
+}
+
+try {
+  if (opt.options.toHtml) {
+
+    var text = fs.readFileSync(opt.options.toHtml,'utf8');
+    var parsed = require('./lib/parser-text').parse(text);
+
+    if (opt.options.parse) {
+      output(parsed);
+    } else {
+      var html = require('./lib/output-html').output(parsed);
+      output(html);
+    }
+    
+  } else if (opt.options.fromHtml) {
+
+    var html = fs.readFileSync(opt.options.fromHtml,'utf8');
+    var parsed = require('./lib/parser-html').parse(html);
+
+    if (opt.options.parse) {
+      output(parsed);
+    } else {
+      var text = require('./lib/output-text').output(parsed);
+      output(text);
+    }
+    
+  } else if (opt.options.oldHtml && opt.options.diff) {
+
+    var html = fs.readFileSync(opt.options.oldHtml,'utf8');
+    var parsed = require('./lib/parser-html').parse(html);
+    
+    var diff = fs.readFileSync(opt.options.diff,'utf8');
+    var changes = require('./lib/parser-diff').parse(diff);
+    var changed = require('./lib/patch').patch(parsed,changes);
+
+    if (opt.options.parse) {
+      output(changed);
+    } else {
+      var html = require('./lib/output-html').output(changed);
+      output(html);
+    }
+  } else {
+    getopt.showHelp();
+  }
+} catch (err) {
+  console.error('ERROR', err.message || err);
 }
 
